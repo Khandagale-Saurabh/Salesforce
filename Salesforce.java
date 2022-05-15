ee06993700e7ddcc107e9b2558d9b48a65eea469
Saurabh Sheshrao Khandagale <59130610+Khandagale-Saurabh@users.noreply.github.com>
2022-05-15 11:51:14 +0530

    Update Salesforce.java

--- Salesforce.java
+++ Salesforce.java
@@ -2,3 +2,13 @@ Object Manager
 1]field : we create filed
 2]compact layout: mobile,tab ki liye kse dekhana hai
 3] 
+
+
+Opportunities are deals in progress. Opportunity records track details about deals
+,including which accounts they're for, who the players are, and the amount of potential sales
+
+WorkFlow Rule Action Type:
+Task:
+Email Alert:
+Field Update:
+Outbound Message:
